486260890fdf5ac48f6930d0c2f10832d22fb264
70231_Pravin <pravininirmal@gmail.com>
2023-07-13 00:57:59 +0530

    abc

---
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace tester;
+using MyFirstProject;
+public class Tester
+{
+    public static void Main(String[] args)
+    {
+
+        Student student = new Student(10, "Privin Nirmal", "pravin@gmail.com", 1520.30);
+        //Console.WriteLine(student);
+
+        //Console.WriteLine(student.Name);
+        //student.Name = "ganesh patil";
+
+        //string s = string.Format("{0},{1},{2}", student.Name, student.id, student.Email);
+
+        //Console.WriteLine(s);
+
+        List<Student> list = new List<Student>();
+        list.Add(student);
+        foreach(Student l in list)
+        {
+            Console.WriteLine(l);
+        }
+
+
+
+    }
+}
---
+++ MyFirstProject/MyFirstProject/Student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    public class Student
+    {
+        internal int id;
+        private string name;
+        private string email;
+        private double salary;
+
+
+        public Student()
+        {
+            Console.WriteLine("default constructor...!!!");
+        }
+
+        public Student(int id, string name, string email, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return "id: " + id + ", name: " + name + ", mail: " + email;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+
+            set
+            {
+                email = value;
+            }
+        }
+
+        public double Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+
+        
+    }
+}
